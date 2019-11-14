47fec81b1415f6329673ad8aa2cfb368ac2c345e
Phong Yang <phongyang88@hotmail.com>
2019-11-14 14:52:06 -0600

    finished task 2

--- index.js
+++ index.js
@@ -74,7 +74,24 @@ class Person {
 */
 
 class Car {
+  constructor(model, milesPerGallon){
+    this.tank = 0;
+    this.odometer = 0;
+    this.model = model;
+    this.milesPerGallon = milesPerGallon
+  }
+  fill(gallons){
+    this.tank += gallons;
+  }
+  drive(distance){
+    if (distance > this.tank*this.milesPerGallon){
+      distance = this.tank*this.milesPerGallon
+    }
+    this.odometer += distance;
+    this.tank -= distance/this.milesPerGallon;
 
+    return `I ran out of fuel at ${this.odometer} miles.`;
+  }
 }
 
 /*
